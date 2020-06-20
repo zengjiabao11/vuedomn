1ca6e9853aff841efe1969189ddee2ee374027f2
zengjiabao <121243800@qq.com>
2020-06-20 18:16:44 +0800

    完成用户列表的开发

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,104 +1,111 @@
 <template>
-  <div class="login">
-    <div class="login_content">
-      <div class="login-img">
-        <img src="../assets/logo.png" alt />
+  <div class="Login">
+    <div class="login-box">
+      <div class="avatar-box">
+         <img src="../assets/logo.png">
+         
       </div>
-      <el-form :model="fromlogin"  :rules="loginrules" ref="loginref" label-width="0px" class="a">
-        <el-form-item prop="username">
-          <el-input v-model="fromlogin.username" prefix-icon=" iconfont icon-ico_username"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            type="password"
-            
-            v-model="fromlogin.password"
-            prefix-icon=" iconfont icon-mima1"
-          ></el-input>
-        </el-form-item>
-        <el-button type="primary" @click="login">登陆</el-button>
-        <el-button type="warning" @click="submmit">重置</el-button>
-      </el-form>
+      <el-form ref="Loginformref" :model="Loginform" :rules="Loginformrules" label-width="0px" class="login_form">
+         <el-form-item prop="username">
+           <el-input prefix-icon="iconfont icon-yonghuming" v-model="Loginform.username"></el-input>
+         </el-form-item>
+
+            <el-form-item prop="password">
+           <el-input prefix-icon="iconfont icon-mima" v-model="Loginform.password" type="password"></el-input>
+         </el-form-item>
+
+        <el-form-item class="btns">
+           <el-button type="primary" @click="login">登陆</el-button>
+           <el-button type="info" @click="resset">重置</el-button>
+         </el-form-item>
+         </el-form>
     </div>
   </div>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      fromlogin: {
-        username: "admin",
-        password: "123456"
+  name: 'Login',
+  data(){
+    return{
+      Loginform:{
+        username:'admin',
+        password:'123456'
       },
-      loginrules: {
-        username: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 5, max: 10, message: "长度在 5 到 10 个字符", trigger: "blur" }
+      Loginformrules:{
+        username:[ 
+        {required:true,message:"请输入登录名称",trigger:"blur"},
+        {min:3,max:10,message:"长度在 3 到 10个字符",trigger:"blur"}
         ],
-        password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 6, max: 12, message: "长度在 6 到 12 个字符", trigger: "blur" }
+        password:[
+          {required:true,message:"请输入密码",trigger:"blur"},
+          {min:6,max:10,message:"长度在 6 到 10个字符",trigger:"blur" }
         ]
       }
-    };
+    }
+    
   },
   methods:{
-    submmit(){
-       this.$refs.loginref.resetFields();
+    resset(){
+      this.$refs.Loginformref.resetFields();
     },
     login(){
-      this.$refs.loginref.validate(async valid=>{
-        if(!valid)return ;
-        const {data:res}= await this.$http.post("login",this.fromlogin)
-        if(res.meta.status !==200){
-           return this.$message.error('登录失败')
-        }
-          this.$message.success('登录成功')
-        window.sessionStorage.getItem('token',res.data.token)
-        this.$router.push("/home")
-      })
+       this.$refs.Loginformref.validate(async valid =>{
+         if(!valid) return;
+      const { data: res} =  await  this.$http.post("login",this.Loginform);
+       if(res.meta.status !==200) return this.$message.error("登陆失败")
+         this.$message.success("登陆成功");
+        window.sessionStorage.setItem("token",res.data.token)  ;
+        this.$router.push("/home")     })
     }
   }
-};
+  
+}
 </script>
-<style  scoped>
-.login {
-  background: #2b4b6b;
-  height: 100%;
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Login{
+    background-color: #2b4b6b;
+    height: 100%;
 }
-.login_content {
-  background: #fff;
-  height: 300px;
-  width: 450px;
+.login-box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top:50%;
+    transform: translate(-50%,-50%)
+}
+.avatar-box{
+    height: 130px;
+    width: 130px;
+     border:1px solid #eee;
+     border-radius: 50%;
+     padding: 10px;
+     box-shadow: 0 0 10px #ddd;
+     position: absolute;
+     left:50%;
+     transform: translate(-50%,-50%);
+     background-color: #fff;
+}
+img{
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+}
+.btns{
+  display: flex;
+  justify-content: flex-end
+}
+.login_form{
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-.login-img {
-  height: 130px;
-  width: 130px;
-  position: absolute;
-  left: 50%;
-  border: 1px solid #eee;
-  border-radius: 50%;
-  padding: 10px;
-  transform: translate(-50%, -50%);
-  background: #fff;
-  box-shadow: 0 0 10px #eee;
-}
-.login-img img {
+  bottom: 0;
   width: 100%;
-  height: 100%;
-  border-radius: 50%;
-  background: #eee;
-}
-.a {
-  position: absolute;
-  bottom: 10%;
-  width: 100%;
-  padding: 0 10px;
-  box-sizing: border-box;
-  text-align: center;
+  padding: 0 20px;
+  box-sizing: border-box
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,37 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import home from '../components/home.vue'
-
+import welcome from '../components/welcome.vue'
+import users from '../components/users/users.vue'
 Vue.use(VueRouter)
 
-  const routes = [
-    {path:'/',redirect:'/login'},
-    {path:'/login',component:Login},
-    {path:'/home',component:home}
+const routes = [{
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: home,
+    children: [{
+      path: '/welcome',
+      component: welcome
+    }, {
+      path: '/home',
+      redirect: '/welcome'
+    },{
+      path: '/users',
+      component: users
+    }]
+  }
 ]
+
 
 const router = new VueRouter({
   routes
 })
-router.beforeEach((to,from,next)=>{
-  if(to.path ==='/login') return next()
-  const tokenstr = window.sessionStorage.getItem('token')
-  if(!tokenstr) return next('/login')
-  next()
-})
+
 export default router
